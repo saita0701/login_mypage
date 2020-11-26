180d80b6dd935153dbd4218797118b65d125f37f
saita0701 <yuya970701@gmail.com>
2020-11-26 13:46:01 +0900

    ログインフォーム

---
+++ login_mypage/login_mypage/mypage_hensyu.php
@@ -0,0 +1,71 @@
+<?php
+mb_internal_encoding("utf8");
+
+if(empty ($_POST['from_mypage'])){
+    header("Location:login_error.php");
+}
+
+session_start();
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="ja">
+    <head>
+    <meta charset="utf-8">
+    <title>マイページ登録</title>
+    <link rel="stylesheet" type="text/css" href="mypage_hensyu.css">
+    </head>
+    
+    <body>
+        <header>
+        <img src="4eachblog_logo.jpg">
+        <div class="logout"><a href="log_out.php">ログアウト</a></div>
+        </header>
+    
+         <div class="box">
+                <h2>会員情報</h2>
+            
+                <div class="hello">
+                    <?php echo "こんにちは！ ".$_SESSION['name']."さん"; ?>
+                </div>
+                <form action="mypage_update.php" method="post">
+                    <div class="profile_pic">
+                        <img src="<?php echo $_SESSION['picture']; ?>">
+                    </div>
+
+                    <div class="basic_info">
+                        <div class="name">
+                            <p>氏名：
+                            <input type="text" size="30" value="<?php echo $_SESSION['name']; ?>" name="name">
+                            </p>
+                        </div>
+                        <div class="mail">
+                            <p>メール：
+                            <input type="text" size="30" value="<?php echo $_SESSION['mail']; ?>"
+                            name = "mail">
+                            </p>
+                        </div>
+                        <div class="password">
+                            <p>パスワード：
+                            <input type="text" size="30" value="<?php echo $_SESSION['password']; ?>"
+                            name = "password">
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="comments">
+                        <textarea rows="3" cols="75" name = "comments"><?php echo $_SESSION['comments']; ?>
+                        </textarea>
+                    </div>
+                    <div class="button_center">
+                    <input type="submit" class="submit_button" size="35" value="この内容に変更する">
+                    </div>
+
+                </form>
+            </div>
+            <footer>
+                 © 2018 InterNous.inc. All rights reserved
+            </footer>
+    </body>
+</html>
